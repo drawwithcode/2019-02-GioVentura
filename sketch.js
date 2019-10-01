16d695fda18b61199eab2bfa566fd912bc4accb9
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-10-01 17:39:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,25 @@
 var colorList = ['#BECAE6',
-                 '#209EBB',
-                 '#023047',
-                 '#FFB701',
-                 '#FC8500']
+  '#209EBB',
+  '#023047',
+  '#FFB701',
+  '#FC8500'
+]
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
 }
 
 function draw() {
-  for (var x = 0; x < windowWidth+10; x += 10) {
-  for (var y = 0; y < windowHeight+10; y += 10) {
+  for (var x = 0; x < windowWidth + 20; x += 20) {
+    for (var y = 0; y < windowHeight + 20; y += 20) {
+      fill(255);
 
-  fill(random(colorList));
-  ellipse(x,y,15);}
+      ellipse(x, y, 30);
+      fill(random(colorList));
+      ellipse(x, y, 20);
+    }
   }
 
 
